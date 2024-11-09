ac031451de4062f820f4c2ec3865d00ad987f07a
Shoirhi <men_09_ahem@icloud.com>
2024-11-10 08:46:49 +0900

    修正

--- src/features/contact/config.ts
+++ src/features/contact/config.ts
@@ -1,9 +1,9 @@
 import type { ContactConfigProps } from "./types";
 
 export const CONTACT_CONFIG: ContactConfigProps = {
-  siteName: "",
-  fromName: "",
-  fromEmail: "",
-  notifyEmail: "",
-  turnstileSiteKey: "",
+  siteName: "サイト名",
+  fromName: "送信者",
+  fromEmail: "contact@send.bolstatech.com",
+  notifyEmail: "info@bolstatech.com",
+  turnstileSiteKey: "1x00000000000000000000AA",
 };
