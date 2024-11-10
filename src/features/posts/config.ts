72206f8ff864c317ab5db0082ea3f0c5c5f504c8
Shoirhi <men_09_ahem@icloud.com>
2024-11-10 09:24:35 +0900

    設定を変更

--- src/features/posts/config.ts
+++ src/features/posts/config.ts
@@ -2,8 +2,8 @@ import type { PostsConfigProps } from "./types";
 
 export const POSTS_CONFIG: PostsConfigProps = {
   postsCMSEndpoint: "posts",
-  postsPerPage: 10,
-  postsForPreview: 5,
+  postsPerPage: 6,
+  postsForPreview: 6,
   postsIndexSlug: "/posts",
   postCategoriesCMSEndpoint: "post-categories",
 };
